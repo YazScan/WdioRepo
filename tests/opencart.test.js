af86597e8e0a4ea9ef3df0d25c2ddfe267da6696
YazScan <Yazscanziani@gmail.com>
2022-11-24 15:27:16 -0300

    added expects (deberia buscar iphone and deberia buscar apple cinema...)

--- tests/opencart.test.js
+++ tests/opencart.test.js
@@ -2,19 +2,21 @@ describe('Carrito', () => {
   
   it('Debería buscar iphone', async ()=> {
     await browser.url('/');
-    let barraDeBusqueda = await $('[name="search"]');
+    let barraDeBusqueda = await $('input[name="search"]');
     await barraDeBusqueda.setValue('iphone');
-    await assert.equal(await barraDeBusqueda.getValue(),'iphone','Error: no se mostró el texto esperado en la barra de búsqueda');
-    await barraDeBusqueda.keys('Enter');            
-    await assert.equal(await $('h4').getText(),'iPhone','Error: no se mostró el header esperado');
-   });
+    await expect(await barraDeBusqueda.getValue()).to.equal('iphone','Error: no se mostró el texto esperado en la barra de búsqueda');
+    await barraDeBusqueda.keys('Enter');
+    await expect(await $('h4').getText()).to.include.string('iPhone','Error: no se mostró el header esperado');
+ });
   
 
     it('Debería buscar apple cinema, ingresar al artículo y seleccionar un color', async () => {
         await browser.url('/')
         let barraDeBusqueda = await $('[name="search"]');
         await barraDeBusqueda.setValue('apple cinema');
+        await expect(await barraDeBusqueda.getValue()).to.equal('apple cinema','Error: no se mostró el texto esperado en la barra de búsqueda');
         await barraDeBusqueda.keys('Enter');
+        await expect(await $('h4').getText()).to.include.string('Apple Cinema 30"','Error: no se mostró el header esperado');
         let articulo = await $('=Apple Cinema 30"');
         await articulo.click();
         let dropDownColor = await $('select');
